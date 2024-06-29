33b58f65a832fb80bf140ba3e6cd9322ac9b6f57
a007mr <anton.cherkasov@gmail.com>
2024-06-29 12:14:08 +0800

    OnchainKit

--- app/page.js
+++ app/page.js
@@ -1,30 +1,30 @@
 import Hero from './components/main/Hero';
 
-// import { getFrameMetadata } from '@coinbase/onchainkit/frame';
+import { getFrameMetadata } from '@coinbase/onchainkit/frame';
 
-// const frameMetadata = getFrameMetadata({
-//   buttons: [
-//     {
-//       label: 'Learn about CHOMP',
-//       action: 'link',
-//       target: 'https://www.chompcoin.xyz/',
-//     },
-//     {
-//       label: 'Chomp Legacy',
-//       action: 'link',
-//       target: 'https://www.chomplegacy.com/',
-//     },
-//   ],
-//   image: `${process.env.NEXT_PUBLIC_URL}/img/og_image.png`,
-//   // postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame`,
-// });
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Learn about CHOMP',
+      action: 'link',
+      target: 'https://www.chompcoin.xyz/',
+    },
+    {
+      label: 'Chomp Legacy',
+      action: 'link',
+      target: 'https://www.chomplegacy.com/',
+    },
+  ],
+  image: `${process.env.NEXT_PUBLIC_URL}/img/og_image.png`,
+  // postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame`,
+});
 
-// export const metadata = {
-//   manifest: '/manifest.json',
-//   other: {
-//     ...frameMetadata
-//   },
-// };
+export const metadata = {
+  manifest: '/manifest.json',
+  other: {
+    ...frameMetadata
+  },
+};
 
 export default function Home() {
   return (
